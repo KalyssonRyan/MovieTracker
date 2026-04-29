859f92cb0289e792f6d3fa03208f8e284d13a870
KalyssonRyan <kalyssonryan@gmail.com>
2026-04-29 14:55:15 -0300

    feat(server): connect to database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const routes = require('./routes');
-
+const {client,ConnectDb} = require('./config/database');
+ConnectDb()
 app.use(express.json()) 
 app.use(cors())        
 app.use(routes)
