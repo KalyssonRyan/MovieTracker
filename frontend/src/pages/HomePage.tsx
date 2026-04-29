d50a966b4be0b78b5ad6fe52f397eb721d982188
KalyssonRyan <kalyssonryan@gmail.com>
2026-04-29 17:37:50 -0300

    feat(HomePage): links

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <>
       <h1>Pagina Principal</h1>
+      <Link to={"/movies"}>Ver Lista de Filmes</Link>
+      <Link to={"/create"}>Adicionar Filme</Link>
     </>
   );
 }
