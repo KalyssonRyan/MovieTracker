35b0de7202f8eed459ba45255a042ac2f8b51e19
KalyssonRyan <kalyssonryan@gmail.com>
2026-04-29 14:54:56 -0300

    feat: database

---
+++ backend/config/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const {Client} = require('pg');
+ 
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+    
+})
+
+async function ConnectDb(){
+    try{
+        await client.connect();
+        console.log("Conectado ao banco")
+    }catch(err){
+        console.log("erro ao conectar", err)
+    }
+}
+module.exports = {client, ConnectDb}
