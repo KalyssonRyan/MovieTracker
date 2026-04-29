415077ecd6af52cf41c0df18b5978e8e54f0a31c
KalyssonRyan <kalyssonryan@gmail.com>
2026-04-29 15:03:57 -0300

    feat(index): get movies

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const {client} = require('../config/database')
 
-router.get('/', (req, res) => {
-  res.send('API funcionando 🚀');
+router.get('/movies', async (req, res) => {
+  try{
+    const result = await client.query("SELECT * FROM movies")
+    return res.status(200).json({
+      data: result.rows
+    })
+  }catch(err){
+    console.log("Ocorreu um erro",err)
+    return res.status(500).json({
+      error: err
+    })
+  }
 });
 
 module.exports = router;
