c060819c3769643157b975cd36975363e4222a9c
KalyssonRyan <kalyssonryan@gmail.com>
2026-04-29 15:14:54 -0300

    feat(index): post movies

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {client} = require('../config/database')
+const { client } = require("../config/database");
 
-router.get('/movies', async (req, res) => {
-  try{
-    const result = await client.query("SELECT * FROM movies")
+router.get("/movies", async (req, res) => {
+  try {
+    const result = await client.query("SELECT * FROM movies");
     return res.status(200).json({
-      data: result.rows
-    })
-  }catch(err){
-    console.log("Ocorreu um erro",err)
+      data: result.rows,
+    });
+  } catch (err) {
+    console.log("Ocorreu um erro", err);
     return res.status(500).json({
-      error: err
+      error: err,
+    });
+  }
+});
+
+router.post("/movies", async (req, res) => {
+  try{
+    const { nome, genero, status, nota, poster } = req.body;
+    
+    const result = await client.query(
+      "INSERT INTO movies(nome,genero,status,nota,poster) VALUES($1,$2,$3,$4,$5) RETURNING *",
+      [nome, genero, status, nota, poster],
+    );
+    return res.status(200).json(result.rows[0]);
+  }catch(err){
+    console.log(err)
+    return res.status(500).json({
+      error: "Erro interno cheque os logs"
     })
   }
 });
