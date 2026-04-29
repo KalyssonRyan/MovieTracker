22ad2a3d0890cd52e53d278d15fd45bcfd80fd53
KalyssonRyan <kalyssonryan@gmail.com>
2026-04-29 17:19:10 -0300

    feat: list movies

--- frontend/src/pages/ListMoviePage.tsx
+++ frontend/src/pages/ListMoviePage.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+
+type Movies = {
+  id: string;
+  nome: string;
+  genero: string;
+  status: string;
+  nota: Number;
+  poster: string;
+};
 function ListMoviePage() {
+  const [movies, setMovies] = useState<Movies[]>([]);
+  useEffect(() => {
+    async function run() {
+      try {
+        const result = await fetch("http://localhost:3000/movies");
+        const data = await result.json();
+        setMovies(data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    run();
+  }, []);
   return (
     <>
       <h1>aqui vai a lista de filmes</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h3>{movie.nome}</h3>
+          <p>{movie.genero}</p>
+          <img src={movie.poster} alt="" />
+        </div>
+      ))}
     </>
   );
 }
