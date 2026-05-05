3c375ce6eec32d55e82193dbde76bbfe661f9c87
KalyssonRyan <kalyssonryan@gmail.com>
2026-05-05 10:30:11 -0300

    feat(routes): implement layout

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,16 +4,19 @@ import ListMoviePage from "./pages/ListMoviePage";
 import AddPage from "./pages/AddPage";
 import EditPage from "./pages/EditPage";
 import SearchPage from "./pages/SearchPage";
+import Layout from "./components/layout";
 
 function RouterList() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movies" element={<ListMoviePage />} />
-        <Route path="/create" element={<AddPage />} />
-        <Route path="/edit/:id" element={<EditPage />} />
-        <Route path="/search/" element={<SearchPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<ListMoviePage />} />
+          <Route path="/create" element={<AddPage />} />
+          <Route path="/edit/:id" element={<EditPage />} />
+          <Route path="/search/" element={<SearchPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
